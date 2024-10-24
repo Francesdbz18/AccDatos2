1a72127a5e06e7aa0d299b9838148b2b7531b564
Francesco Fevoli <francesdbz18pc@outlook.es>
2024-10-24 10:33:16 +0200

    Xonga

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+
+public class Main {
+    static String BDPer = "DBPersonas.yap";
+
+    public static void main(String[] args) {
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), BDPer);
+        Persona pl = new Persona("Juan", "Guadalajara");
+        Persona p2 = new Persona("Ana", "Madrid");
+        Persona p3 = new Persona("Luis", "Granada");
+        Persona p4 = new Persona("Pedro", "Asturias");
+        db.store(pl);
+        db.store(p2);
+        db.store(p3);
+        db.store(p4);
+        db.close();
+    }
+}
---
+++ src/main/java/org/example/Persona.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Persona {
+    private String nombre;
+    private String ciudad;
+
+    public Persona(String nombre, String ciudad) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+}
